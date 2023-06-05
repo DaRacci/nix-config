ff8ae2da18668315dcab86251fe0d002017ccd89
DaRacci <me@racci.dev>
2023-06-05 19:13:31 +1000

    fix: add .direnv to git ignore

--- home/racci/global/features/cli/direnv.nix
+++ home/racci/global/features/cli/direnv.nix
@@ -1,6 +1,10 @@
-{
+{ lib, config, ... }: {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+  };
+
+  programs.git = lib.mkIf config.programs.git.enable {
+    ignores = [ ".direnv" ];
   };
 }
