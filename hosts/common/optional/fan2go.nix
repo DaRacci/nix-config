a34265582d8b142f7f825f98567023f122710ccc
DaRacci <me@racci.dev>
2023-06-13 23:07:20 +1000

    feat: fan2go daemon file & that shit works

--- hosts/common/optional/fan2go.nix
+++ hosts/common/optional/fan2go.nix
@@ -1,5 +1,25 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   environment.systemPackages = with pkgs; [
     fan2go
+    lm_sensors
+  ];
+
+  systemd.services.fan2god = {
+    description = "Fan2Go Daemon";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      LimitNOFILE = 8192;
+      ExecStart = "${pkgs.fan2go}/bin/fan2go -c /etc/fan2go/fan2go.yaml --no-style";
+      Restart = "always";
+      RestartSec = "1s";
+      StandardOutput = "null";
+    };
+    environment = {
+      PATH = lib.mkForce "${pkgs.procps}/bin";
+    };
+  };
+
+  environment.persistence."/persist".directories = [
+    "/etc/fan2go"
   ];
 }
