355045abd8f82374e1c68c80ed9bdec8a9609a0e
DaRacci <me@racci.dev>
2023-06-13 23:08:59 +1000

    feat: bottles with persistent dir

--- home/racci/global/features/desktop/common/wine.nix
+++ home/racci/global/features/desktop/common/wine.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     winePackages.stagingFull
+    bottles
+  ];
+
+  home.persistence."/persist/home/racci".directories = [
+    ".local/share/bottles"
   ];
 }
