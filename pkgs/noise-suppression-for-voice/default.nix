e141d1aab2d8a54041f3bad1449ed436835368ab
DaRacci <me@racci.dev>
2023-06-13 23:05:51 +1000

    chore: remove that shit

--- pkgs/noise-suppression-for-voice/default.nix
+++ pkgs/noise-suppression-for-voice/default.nix
@@ -30,22 +30,6 @@ stdenv.mkDerivation rec {
     webkitgtk
   ];
 
-  # libasound2-dev libjack-jackd2-dev \
-  #   libcurl4-openssl-dev  \
-  #   libfreetype6-dev \
-  #   libx11-dev libxcomposite-dev libxcursor-dev libxcursor-dev libxext-dev libxinerama-dev libxrandr-dev libxrender-dev \
-  #   libwebkit2gtk-4.0-dev
-
-# - Checking for module 'alsa'
-# --   No package 'alsa' found
-# -- Checking for module 'freetype2'
-# --   No package 'freetype2' found
-# -- Checking for module 'libcurl'
-# --   Found libcurl, version 8.0.1
-# -- Checking for modules 'webkit2gtk-4.0;gtk+-x11-3.0'
-# --   No package 'webkit2gtk-4.0' found
-# --   No package 'gtk+-x11-3.0' found
-
   meta = with lib; {
     description = "Noise suppression plugin based on Xiph's RNNoise";
     homepage = "https://github.com/werman/noise-suppression-for-voice";
