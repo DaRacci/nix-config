80714d9f3e1966a0096f5ce1487ec77dbc0ff567
DaRacci <me@racci.dev>
2023-07-01 23:16:13 +1000

    chore: actually import those

--- home/racci/global/features/cli/default.nix
+++ home/racci/global/features/cli/default.nix
@@ -4,12 +4,14 @@
     ./bat.nix
     ./bottom.nix
     ./broot.nix
+    ./carapace.nix
     ./direnv.nix
     ./git.nix
     # ./gpg.nix
     ./keyring.nix
     ./micro.nix
     ./nushell.nix
+    ./ripgrep.nix
     ./ssh.nix
     ./starship.nix
     ./xplr.nix
--- home/racci/global/features/cli/nushell.nix
+++ home/racci/global/features/cli/nushell.nix
@@ -6,9 +6,23 @@
       NODE_SKIP_PLATFORM_CHECK = "1"; # FUCK YOU COPILOT!
     };
 
-    # configFile.text = ''
-    # ${ builtins.tryEval (${pkgs.carapace}/bin/carapace _carapace) }
-    # '';
+    extraEnv = ''
+    def-env load-sh-env [path: path] {
+      let columns = (open $path
+        | lines
+        | filter {|line| (not ($line | is-empty)) and ($line | str starts-with "export ") } # Only keep actual variables
+        | each {|line| $line | str substring 7..}                                           # Remove 'export '
+        | split column '=' name value)                                                      # Split into columns
+
+      for element in $columns {
+        let value = ($element.value | str trim -c '"')
+        let-env $element.name = $value
+      } 
+    }
+    '';
+
+    extraConfig = ''
+    '';
   };
 
   programs.zoxide = lib.mkIf config.programs.zoxide.enable {
