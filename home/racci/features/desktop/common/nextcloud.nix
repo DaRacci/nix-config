14fa8e9544ea550a9ab4aa3db68be9fe3fb869ad
DaRacci <me@racci.dev>
2023-07-23 16:31:18 +1000

    fix: nextcloud on wayland

--- home/racci/features/desktop/common/nextcloud.nix
+++ home/racci/features/desktop/common/nextcloud.nix
@@ -1,5 +1,8 @@
-{ pkgs, config, ... }: {
-  home.packages = with pkgs; [ nextcloud-client ];
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    nextcloud-client # Required to be able to use the nextcloud client and not only as a background service
+    libsForQt5.qt5.qtwayland # Required for the nextcloud client to work on wayland
+  ];
 
   services.nextcloud-client = {
     enable = true;
