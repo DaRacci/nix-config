d77a82ffbb6549cd45d8435f3696824cc9a3e8df
James Draycott <me@racci.dev>
2023-10-11 15:31:27 +1100

    fix: only in unstable branc

--- home/racci/features/desktop/development/jetbrains.nix
+++ home/racci/features/desktop/development/jetbrains.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ... }: {
   home.packages = with pkgs; [
     jetbrains.idea-community
-    jetbrains.rust-rover
+    unstable.jetbrains.rust-rover
   ];
 
   programs.git = lib.mkIf config.programs.git.enable {
