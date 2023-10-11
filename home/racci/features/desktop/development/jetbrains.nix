02e798ccebd5769f8a84107fa6741dc95408bd9c
James Draycott <me@racci.dev>
2023-10-11 15:30:40 +1100

    chore: no longer needed, pr was merged

--- home/racci/features/desktop/development/jetbrains.nix
+++ home/racci/features/desktop/development/jetbrains.nix
@@ -1,21 +1,4 @@
 { pkgs, lib, config, ... }: {
-  nixpkgs.overlays =
-    let
-      owner = "Followin";
-      branchname = "jetbrains-rust-rover-init"; # branchname or rev
-      pkgsReview = pkgs.fetchzip {
-        url = "https://github.com/${owner}/nixpkgs/archive/${branchname}.tar.gz";
-        # Change to 52 zeroes when the archive needs to be redownloaded
-        sha256 = "sha256-gdcoP0yTmFjlQOMoqxFSZnhQf/41fnx2XnQWOReaR7Q=";
-      };
-    in
-    [
-      (self: super: {
-        # review = import pkgsReview { overlays = [ ]; config = super.config; };
-        jetbrains = (import pkgsReview { overlays = [ ]; config = super.config; }).jetbrains;
-      })
-    ];
-
   home.packages = with pkgs; [
     jetbrains.idea-community
     jetbrains.rust-rover
