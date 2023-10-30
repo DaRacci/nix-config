9acf7b3673674847265b30abf2dc940f803a4682
DaRacci <me@racci.dev>
2023-10-29 20:22:30 -0700

    feat: add hm applications to windows start menu

--- hosts/common/optional/wsl.nix
+++ hosts/common/optional/wsl.nix
@@ -1,13 +1,28 @@
-{
+{ config, pkgs, lib, ... }: with builtins; with lib; {
   wsl.enable = true;
   wsl.defaultUser = "racci";
   wsl.startMenuLaunchers = true;
   wsl.nativeSystemd = true;
   users.allowNoPasswordLogin = true;
 
-  # wsl.wslConf.automount.mountFsTab = true;
-  # wsl.wslConf.boot.command = ''/mnt/c/Windows/system32/schtasks.exe /run /tn "mount-wsl-disks"'';
-  wsl.wslConf.interop.enabled = false;
-  wsl.wslConf.interop.appendWindowsPath = false;
+  wsl.wslConf.interop.enabled = true;
+  wsl.wslConf.interop.appendWindowsPath = true;
 
+  # Fixes Home-Manager applications not appearing in Start Menu
+  system.activationScripts.copy-user-launchers = stringAfter [ ] ''
+    for x in applications icons; do
+      echo "setting up /usr/share/''${x}..."
+      targets=()
+      if [[ -d "/home/${config.wsl.defaultUser}/.nix-profile/share/$x" ]]; then
+        targets+=("/home/${config.wsl.defaultUser}/.nix-profile/share/$x/.")
+      fi
+
+      if (( ''${#targets[@]} != 0 )); then
+        mkdir -p "/usr/share/$x"
+        ${pkgs.rsync}/bin/rsync -ar --delete-after "''${targets[@]}" "/usr/share/$x"
+      else
+        rm -rf "/usr/share/$x"
+      fi
+    done
+  '';
 }
