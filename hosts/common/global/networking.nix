ffbcd661afd8ffb98b562602d437c19abfe86331
DaRacci <me@racci.dev>
2023-12-15 23:34:58 +0000

    chore: remove explicit dns and enable ipv6

--- hosts/common/global/networking.nix
+++ hosts/common/global/networking.nix
@@ -1,7 +1,6 @@
 { ... }: {
   networking = {
-    enableIPv6 = false; #? TODO :: Learn IPv6 
-    nameservers = [ "9.9.9.9" "1.1.1.1" "1.0.0.1" ];
+    enableIPv6 = true;
 
     firewall = {
       # Wireguard Fix
