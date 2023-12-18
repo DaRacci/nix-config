19ff67d6257dd784ca4923084719e158c9da79ad
DaRacci <me@racci.dev>
2023-12-17 18:03:10 -0800

    fix: use updated rust-rover

--- home/racci/features/desktop/development/jetbrains.nix
+++ home/racci/features/desktop/development/jetbrains.nix
@@ -1,7 +1,23 @@
 { pkgs, lib, config, ... }: {
+  nixpkgs.overlays =
+    let
+      owner = "DaRacci";
+      branchname = "jetbrains-update";
+      pkgsReview = pkgs.fetchzip {
+        url = "https://github.com/${owner}/nixpkgs/archive/${branchname}.tar.gz";
+        # Change to 52 zeroes when the archive needs to be redownloaded
+        sha256 = "sha256-M/KzmDbQeFWeuyL0Dj9eu8KAmM9ae78AD5D1FNnDJSE=";
+      };
+    in
+    [
+      (self: super: {
+        jetbrains = (import pkgsReview { overlays = [ ]; config = super.config; }).jetbrains;
+      })
+    ];
+
   home.packages = with pkgs; [
-    jetbrains.idea-community
-    unstable.jetbrains.rust-rover
+    # jetbrains.idea-community
+    jetbrains.rust-rover
   ];
 
   programs.git = lib.mkIf config.programs.git.enable {
