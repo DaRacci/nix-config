bf7af630db620d5983d66d77e837e772c041eded
DaRacci <me@racci.dev>
2024-01-04 02:03:27 +0000

    fix: 1password at system level

--- home/racci/features/desktop/common/secrets.nix
+++ home/racci/features/desktop/common/secrets.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     gnome-secrets
+    _1password-gui
+    _1password
   ];
 
   user.persistence = {
--- hosts/common/global/passwords.nix
+++ hosts/common/global/passwords.nix
@@ -1,16 +1,16 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   hmUsers = builtins.filter (user: (builtins.hasAttr user config.home-manager.users)) (builtins.attrNames config.users.users);
   hasPackage = pkg: username: builtins.elem pkg config.home-manager.users.${username}.home.packages;
   usersWithPackage = pkg: builtins.filter (username: hasPackage pkg username) hmUsers;
 
-  opGuiUsers = builtins.length (usersWithPackage "_1password-gui");
-  opCliUsers = builtins.length (usersWithPackage "_1password");
+  opGuiUsers = usersWithPackage pkgs._1password-gui;
+  opCliUsers = usersWithPackage pkgs._1password;
 in
 {
-  programs._1password.enable = opCliUsers > 0;
+  programs._1password.enable = (builtins.length opCliUsers) > 0;
   programs._1password-gui = {
-    enable = opGuiUsers > 0;
-    polkitPolicyOwners = (builtins.attrNames config.users.users);
+    enable = (builtins.length opGuiUsers) > 0;
+    polkitPolicyOwners = (opGuiUsers);
   };
 }
