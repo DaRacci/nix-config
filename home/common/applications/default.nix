b8eb0db0dfa7846f878c0f18bef6da7b4cd004c2
DaRacci <me@racci.dev>
2024-02-15 05:08:23 +0000

    fix: missing discord and stuff

--- home/common/applications/default.nix
+++ home/common/applications/default.nix
@@ -1,9 +1,14 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./email.nix
     ./image-editor.nix
     ./media.nix
     ./notes.nix
     ./recording.nix
+    ./social.nix
+  ];
+
+  home.packages = with pkgs; [
+    toybox
   ];
 }
