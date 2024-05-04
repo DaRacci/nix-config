433adcae2150571536711056da6bc56c6255650d
DaRacci <me@racci.dev>
2024-05-04 15:48:23 +1000

    fix(hyprland): dbus and xdg portal fixes

--- hosts/shared/optional/hyprland.nix
+++ hosts/shared/optional/hyprland.nix
@@ -2,11 +2,54 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
   };
 
-  services.xserver.updateDbusEnvironment = true;
+  hardware.opengl =
+    let
+      hyprland-packages = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.system};
+    in
+    {
+      package = hyprland-packages.mesa.drivers;
+
+      # if you also want 32-bit support (e.g for Steam)
+      driSupport32Bit = true;
+      package32 = hyprland-packages.pkgsi686Linux.mesa.drivers;
+    };
+
+  services = {
+    xserver.updateDbusEnvironment = true;
+    gnome.gnome-keyring.enable = true;
+  };
+
   security.pam.services.hyprland = {
     enableGnomeKeyring = true;
+  };
+
+  # TODO - Move the HM when 24.05 is released
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+
+    config = {
+      common = {
+        default = [
+          "hyprland"
+          "gtk"
+        ];
+
+        "org.freedesktop.impl.portal.Secret" = [
+          "gnome-keyring"
+        ];
+      };
+    };
+
+    wlr = {
+      enable = false;
+      settings = { };
+    };
   };
 }
