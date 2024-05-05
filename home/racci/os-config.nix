0de81cbeb68ef8df297ce5a5dd1dff0e0f556016
DaRacci <me@racci.dev>
2024-05-05 16:54:15 +1000

    fix: add nushell to shells

--- home/racci/os-config.nix
+++ home/racci/os-config.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   users.users.racci = {
     uid = 1000;
     shell = pkgs.nushell;
   };
+
+  environment.shells = [ config.users.users.racci.shell ];
 }
