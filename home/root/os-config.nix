78ad39fdc64f4b5cffa7b988c62e01f5ab809e4f
DaRacci <me@racci.dev>
2024-06-19 03:33:49 +0000

    chore: allow me to access all root users

--- home/root/os-config.nix
+++ home/root/os-config.nix
@@ -1,8 +1,12 @@
-{ pkgs, lib, ... }:
+{ flake, pkgs, lib, ... }:
 let inherit (lib) mkForce; in {
   users.users.root = {
     shell = pkgs.fish;
     isNormalUser = mkForce false;
+
+    openssh.authorizedKeys.keyFiles = [
+      "${flake}/home/racci/id_ed25519.pub"
+    ];
   };
 
   programs.fish.enable = true;
