6d1789cd3cc1c2838a63f77a224986a74b470ac3
DaRacci <me@racci.dev>
2024-07-14 05:05:55 +0000

    feat: rofi theme

--- home/racci/features/desktop/hyprland/rofi.nix
+++ home/racci/features/desktop/hyprland/rofi.nix
@@ -17,20 +17,7 @@
     ];
 
     cycle = true;
-    location = "top-left";
+    location = "center";
     terminal = lib.getExe config.programs.alacritty.package;
-
-    theme = {
-      "*" = {
-        main-bg = "#24283be6";
-        main-fg = "#c0caf5ff";
-        main-br = "#bb9af7ff";
-        main-ex = "#7dcfffcc";
-        select-bg = "#7aa2f7ff";
-        select-fg = "#24283bff";
-        separatorcolor = "transparent";
-        border-color = "transparent";
-      };
-    };
   };
 }
--- modules/home-manager/custom/theme.nix
+++ modules/home-manager/custom/theme.nix
@@ -1,4 +1,8 @@
-{ config, inputs, pkgs, lib, ... }: with lib; let cfg = config.custom.theme; in {
+{ nix-colors, config, inputs, pkgs, lib, ... }: with lib; let
+  inherit (nix-colors.lib.conversions) hexToRGB;
+  cfg = config.custom.theme;
+in
+{
   imports = [
     inputs.nix-colours.homeManagerModules.default
   ];
@@ -85,46 +89,202 @@
         "hyprctl setcursor ${cfg.cursor.name} ${toString cfg.cursor.size}"
       ];
 
-      programs.alacritty.settings = mkIf config.programs.alacritty.enable {
-        colors = {
-          draw_bold_text_with_bright_colors = true;
+      programs = {
+        alacritty.settings = mkIf config.programs.alacritty.enable {
+          colors = {
+            draw_bold_text_with_bright_colors = true;
 
-          primary = {
-            background = colorScheme.base00;
-            foreground = colorScheme.base05;
-          };
+            primary = {
+              background = colorScheme.base00;
+              foreground = colorScheme.base05;
+            };
 
-          cursor = {
-            text = colorScheme.base00;
-            cursor = colorScheme.base05;
-          };
+            cursor = {
+              text = colorScheme.base00;
+              cursor = colorScheme.base05;
+            };
 
-          normal = {
-            black = colorScheme.base00;
-            red = colorScheme.base08;
-            green = colorScheme.base0B;
-            yellow = colorScheme.base0A;
-            blue = colorScheme.base0D;
-            magenta = colorScheme.base0E;
-            cyan = colorScheme.base0C;
-            white = colorScheme.base05;
-          };
+            normal = {
+              black = colorScheme.base00;
+              red = colorScheme.base08;
+              green = colorScheme.base0B;
+              yellow = colorScheme.base0A;
+              blue = colorScheme.base0D;
+              magenta = colorScheme.base0E;
+              cyan = colorScheme.base0C;
+              white = colorScheme.base05;
+            };
 
-          bright = {
-            black = colorScheme.base03;
-            red = colorScheme.base08;
-            green = colorScheme.base0B;
-            yellow = colorScheme.base0A;
-            blue = colorScheme.base0D;
-            magenta = colorScheme.base0E;
-            cyan = colorScheme.base0C;
-            white = colorScheme.base05;
-          };
+            bright = {
+              black = colorScheme.base03;
+              red = colorScheme.base08;
+              green = colorScheme.base0B;
+              yellow = colorScheme.base0A;
+              blue = colorScheme.base0D;
+              magenta = colorScheme.base0E;
+              cyan = colorScheme.base0C;
+              white = colorScheme.base05;
+            };
 
-          indexed_colors = {
-            index = 16;
-            color = colourScheme.base09;
+            indexed_colors = {
+              index = 16;
+              color = colourScheme.base09;
+            };
           };
+        };
+
+        # Based on https://github.com/tinted-theming/base16-rofi/blob/0353f5c9e1ba2efa4422f05f416ea87e5d0521b8/templates/default.mustache
+        rofi = {
+          theme =
+            let
+              inherit (lib.formats.rasi) mkLiteral;
+              toRGBA = hex: alpha: let rgb = hexToRGB hex; in mkLiteral "rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${alpha}%)";
+            in
+            ''
+              * {
+                red = ${toRGBA colorScheme.base08 100};
+                blue = ${toRGBA colorScheme.base0D 100};
+                lightfg = ${toRGBA colorScheme.base06 100};
+                lightbg = ${toRGBA colorScheme.base01 100};
+                foreground = ${toRGBA colorScheme.base05 100};
+                background = ${toRGBA colorScheme.base00 100};
+                backgronud-color = ${toRGBA colorScheme.base00 0};
+
+                separatorcolor:              @foreground;
+                border-color:                @foreground;
+                selected-normal-foreground:  @lightbg;
+                selected-normal-background:  @lightfg;
+                selected-active-foreground:  @background;
+                selected-active-background:  @blue;
+                selected-urgent-foreground:  @background;
+                selected-urgent-background:  @red;
+                normal-foreground:           @foreground;
+                normal-background:           @background;
+                active-foreground:           @blue;
+                active-background:           @background;
+                urgent-foreground:           @red;
+                urgent-background:           @background;
+                alternate-normal-foreground: @foreground;
+                alternate-normal-background: @lightbg;
+                alternate-active-foreground: @blue;
+                alternate-active-background: @lightbg;
+                alternate-urgent-foreground: @red;
+                alternate-urgent-background: @lightbg;
+                spacing:                     2;
+              }
+              window {
+                  background-color: @background;
+                  border:           1;
+                  padding:          5;
+              }
+              mainbox {
+                  border:           0;
+                  padding:          0;
+              }
+              message {
+                  border:           1px dash 0px 0px ;
+                  border-color:     @separatorcolor;
+                  padding:          1px ;
+              }
+              textbox {
+                  text-color:       @foreground;
+              }
+              listview {
+                  fixed-height:     0;
+                  border:           2px dash 0px 0px ;
+                  border-color:     @separatorcolor;
+                  spacing:          2px ;
+                  scrollbar:        true;
+                  padding:          2px 0px 0px ;
+              }
+              element-text, element-icon {
+                  background-color: inherit;
+                  text-color:       inherit;
+              }
+              element {
+                  border:           0;
+                  padding:          1px ;
+              }
+              element normal.normal {
+                  background-color: @normal-background;
+                  text-color:       @normal-foreground;
+              }
+              element normal.urgent {
+                  background-color: @urgent-background;
+                  text-color:       @urgent-foreground;
+              }
+              element normal.active {
+                  background-color: @active-background;
+                  text-color:       @active-foreground;
+              }
+              element selected.normal {
+                  background-color: @selected-normal-background;
+                  text-color:       @selected-normal-foreground;
+              }
+              element selected.urgent {
+                  background-color: @selected-urgent-background;
+                  text-color:       @selected-urgent-foreground;
+              }
+              element selected.active {
+                  background-color: @selected-active-background;
+                  text-color:       @selected-active-foreground;
+              }
+              element alternate.normal {
+                  background-color: @alternate-normal-background;
+                  text-color:       @alternate-normal-foreground;
+              }
+              element alternate.urgent {
+                  background-color: @alternate-urgent-background;
+                  text-color:       @alternate-urgent-foreground;
+              }
+              element alternate.active {
+                  background-color: @alternate-active-background;
+                  text-color:       @alternate-active-foreground;
+              }
+              scrollbar {
+                  width:            4px ;
+                  border:           0;
+                  handle-color:     @normal-foreground;
+                  handle-width:     8px ;
+                  padding:          0;
+              }
+              sidebar {
+                  border:           2px dash 0px 0px ;
+                  border-color:     @separatorcolor;
+              }
+              button {
+                  spacing:          0;
+                  text-color:       @normal-foreground;
+              }
+              button selected {
+                  background-color: @selected-normal-background;
+                  text-color:       @selected-normal-foreground;
+              }
+              inputbar {
+                  spacing:          0px;
+                  text-color:       @normal-foreground;
+                  padding:          1px ;
+                  children:         [ prompt,textbox-prompt-colon,entry,case-indicator ];
+              }
+              case-indicator {
+                  spacing:          0;
+                  text-color:       @normal-foreground;
+              }
+              entry {
+                  spacing:          0;
+                  text-color:       @normal-foreground;
+              }
+              prompt {
+                  spacing:          0;
+                  text-color:       @normal-foreground;
+              }
+              textbox-prompt-colon {
+                  expand:           false;
+                  str:              ":";
+                  margin:           0px 0.3000em 0.0000em 0.0000em ;
+                  text-color:       inherit;
+              }
+            '';
         };
       };
     }
