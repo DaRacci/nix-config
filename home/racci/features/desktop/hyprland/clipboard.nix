a7e3ab93ffcfac2f23e0bdd6cbcfbd4caac00b8e
DaRacci <me@racci.dev>
2024-07-20 16:58:13 +1000

    fix(Hyprland): rofi clipboard

--- home/racci/features/desktop/hyprland/clipboard.nix
+++ home/racci/features/desktop/hyprland/clipboard.nix
@@ -25,10 +25,20 @@ let inherit (lib) getExe getExe'; in {
     executable = true;
     source = "${pkgs.writeShellApplication {
       name = "cliphist-rofi-img";
-      runtimeInputs = [ config.services.cliphist.package pkgs.gawk ];
-      text = builtins.readFile (builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/sentriz/cliphist/master/contrib/cliphist-rofi-img";
-      });
+      runtimeInputs = with pkgs; [
+        config.services.cliphist.package
+        config.programs.rofi.package
+        gawk
+        wl-clipboard
+        imagemagick
+      ];
+      bashOptions = [];
+      excludeShellChecks = [ "SC2086" ];
+      text = lib.trivial.pipe "https://raw.githubusercontent.com/sentriz/cliphist/master/contrib/cliphist-rofi-img" [
+        builtins.fetchurl
+        builtins.readFile
+      ];
     }}/bin/cliphist-rofi-img";
   };
 }
+
