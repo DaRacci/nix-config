d868a9fa8af1d4eeab4309894bba6bc33ba43126
DaRacci <me@racci.dev>
2024-08-09 13:25:45 +0000

    fix: use perm link for wallpaper

--- hosts/shared/optional/stylix.nix
+++ hosts/shared/optional/stylix.nix
@@ -7,8 +7,8 @@
     enable = true;
     # Image is needed for now, until https://github.com/danth/stylix/issues/200 is fixed.
     image = pkgs.fetchurl {
-      url = "https://initiate.alphacoders.com/images/131/cropped-5120-2880-1319907.jpeg";
-      sha256 = "sha256-0sTnxz+R7sgrSq98ng7drdZLHwDsb5B9I3eQUfjiP7E=";
+      url = "https://nextcloud.racci.dev/s/Hy8qkAWYwqSTjKp/download/17.jpeg";
+      sha256 = "sha256-dYkMPG/kTs0zgwHDFGkzN4gwFqefDWfMC9RUSBVezXE=";
     };
   };
 }
