5019bdf02f3c7cdbe159bac5081a487d450899be
DaRacci <me@racci.dev>
2024-10-18 23:54:42 +1100

    fix: freezing tests
    https://github.com/NixOS/nixpkgs/issues/349053

--- home/shared/applications/recording.nix
+++ home/shared/applications/recording.nix
@@ -4,7 +4,7 @@
     package = pkgs.obs-studio;
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs
-      obs-vkcapture
+      # obs-vkcapture
       obs-pipewire-audio-capture
       # obs-backgroundremoval
       looking-glass-obs
