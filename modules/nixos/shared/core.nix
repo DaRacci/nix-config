349cdc8d60b214f27151fed0542f398e23552a1a
DaRacci <me@racci.dev>
2024-11-06 14:21:26 +1100

    fix: was causing popping audio issues

--- modules/nixos/shared/core.nix
+++ modules/nixos/shared/core.nix
@@ -40,14 +40,6 @@ in
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = false;
-        extraConfig.pipewire."92-low-latency" = {
-          "context.properties" = {
-            "default.clock.rate" = 44100;
-            "default.clock.quantum" = 512;
-            "default.clock.min-quantum" = 512;
-            "default.clock.max-quantum" = 512;
-          };
-        };
       };
 
       services.udev.extraRules = ''
