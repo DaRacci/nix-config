dd7bd823f81a673e8df585d8ae0b0821908dc800
DaRacci <me@racci.dev>
2024-11-07 19:42:45 +1100

    fix: use lix for devenv too

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
 
         overlays = [
           inputs.hyprland-contrib.overlays.default
+          inputs.lix-module.overlays.default
         ] ++ (builtins.attrValues (import ./overlays { inherit self inputs lib; }));
       };
 
@@ -241,7 +242,8 @@
     impermanence.url = "github:nix-community/impermanence";
     lanzaboote = { url = "github:nix-community/lanzaboote/v0.3.0"; inputs = { nixpkgs.follows = "nixpkgs"; flake-parts.follows = "flake-parts"; flake-compat.follows = "flake-compat"; }; };
     nixd = { url = "github:nix-community/nixd"; inputs = { nixpkgs.follows = "nixpkgs"; flake-parts.follows = "flake-parts"; }; };
-    lix-module = { url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz"; inputs.nixpkgs.follows = "nixpkgs"; };
+    lix = { url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz"; flake = false; };
+    lix-module = { url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz"; inputs = { nixpkgs.follows = "nixpkgs"; lix.follows = "lix"; }; };
     stylix = { url = "github:danth/stylix"; };
     nix-alien = { url = "github:thiagokokada/nix-alien"; };
 
