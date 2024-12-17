c1c2c199e34ff97ce33ff5624e65489d27a9acaa
DaRacci <me@racci.dev>
2024-12-17 05:48:12 +0000

    fix(nixai): environment variable definitions use attribute set syntax not a string

--- hosts/server/nixai/default.nix
+++ hosts/server/nixai/default.nix
@@ -10,23 +10,23 @@
     open-webui = {
       enable = true;
       openFirewall = true;
-      environment = ''
-        OLLAMA_BASE_URLS=http://nixe:11434
+      environment = {
+        OLLAMA_BASE_URLS = "http://nixe:11434";
 
-        PDF_EXTRACT_IMAGES=true
-        ENABLE_RAG_LOCAL_WEB_FETCH=true
-        RAG_EMBEDDING_MODEL_AUTO_UPDATE=true
-        RAG_RERANKING_MODEL_AUTO_UPDATE=true
+        PDF_EXTRACT_IMAGES = true;
+        ENABLE_RAG_LOCAL_WEB_FETCH = true;
+        RAG_EMBEDDING_MODEL_AUTO_UPDATE = true;
+        RAG_RERANKING_MODEL_AUTO_UPDATE = true;
 
-        ENABLE_RAG_WEB_SEARCH=true
-        ENABLE_SEARCH_QUERY=true
-        RAG_WEB_SEARCH_ENGINE=searxng
-        SEARXNG_QUERY_URL=http://localhost:9000/search?q=<query>
+        ENABLE_RAG_WEB_SEARCH = true;
+        ENABLE_SEARCH_QUERY = true;
+        RAG_WEB_SEARCH_ENGINE = "searxng";
+        SEARXNG_QUERY_URL = "http://localhost:9000/search?q=<query>";
 
-        AUDIO_STT_MODEL=whisper-1
-        WHISPER_MODEL=base
-        WHISPER_MODEL_AUTO_UPDATE=true
-      '';
+        AUDIO_STT_MODEL = "whisper-1";
+        WHISPER_MODEL = "base";
+        WHISPER_MODEL_AUTO_UPDATE = true;
+      };
     };
 
     searx = {
