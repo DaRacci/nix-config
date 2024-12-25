75596065a810aa18d0308fbc938d92ba96a94c7b
DaRacci <me@racci.dev>
2024-12-25 19:40:46 +1100

    fix(nixcloud): good lord im stupid

--- hosts/server/nixcloud/default.nix
+++ hosts/server/nixcloud/default.nix
@@ -47,7 +47,7 @@
   services = rec {
     immich = {
       enable = true;
-      host = "127.0.0.1";
+      host = "0.0.0.0";
       secretsFile = config.sops.secrets."IMMICH/ENV".path;
       environment = {
         IMMICH_TRUSTED_PROXIES = "100.64.0.0/10,192.168.1.0/24,192.168.2.0/24";
@@ -91,7 +91,7 @@
         dbtype = "pgsql";
         dbuser = "nextcloud";
         dbname = "nextcloud";
-        dbhost = "/run/postgresql";
+        dbhost = "nixio";
         dbpassFile = config.sops.secrets."POSTGRES/NEXTCLOUD_PASSWORD".path;
 
         objectstore.s3 = {
