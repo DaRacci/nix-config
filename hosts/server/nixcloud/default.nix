371486456af27a3656607d7a3f04ee722cf12c40
DaRacci <me@racci.dev>
2024-12-26 22:37:23 +1100

    fix(nixcloud): mount the nextcloud folder for immich
    also fix the executable for fuse.

--- hosts/server/nixcloud/default.nix
+++ hosts/server/nixcloud/default.nix
@@ -249,10 +249,10 @@
     };
   };
 
-  fileSystems."immich" = {
-    device = "immich";
-    mountPoint = "/mnt/immich";
-    fsType = "fuse./run/current-system/sw/bin/s3fs";
+  fileSystems."nextcloud" = {
+    device = "nextcloud";
+    mountPoint = "/var/lib/immich/ext/nextcloud";
+    fsType = "fuse#${lib.getExe pkgs.s3fs}";
     noCheck = true;
     options = [ "_netdev" "allow_other" "use_path_request_style" "url=https://minio.racci.dev" "passwd_file=${config.sops.secrets."IMMICH/S3FS_AUTH".path}" "umask=0007" "mp_umask=0007" "nonempty" "uid=${config.users.users.immich.uid}" "gid=${config.users.groups.immich.gid}" ];
   };
