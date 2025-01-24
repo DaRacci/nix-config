cc0fced50742d5f9e207697c6dfd945707e96600
DaRacci <me@racci.dev>
2025-01-24 14:55:50 +1100

    feat(gaming): add nexus & r2modman support

--- modules/home-manager/purpose/gaming/modding.nix
+++ modules/home-manager/purpose/gaming/modding.nix
@@ -1,16 +1,35 @@
-{ config, pkgs, lib, ... }: with lib; let cfg = config.purpose.gaming.modding; in {
+{ config
+, pkgs
+, lib
+, ...
+}:
+with lib;
+let
+  cfg = config.purpose.gaming.modding;
+in
+{
   options.purpose.gaming.modding = {
     enable = mkEnableOption "Enable modding support";
     enableSatisfactory = mkEnableOption "Enable satisfactory modding support";
     enableBeatSaber = mkEnableOption "Enable beatsaber modding support";
     enableThunderstore = mkEnableOption "Enable thunderstore support";
+    enableNexus = mkEnableOption "Enable nexus mod manager support";
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs;
+    home.packages =
+      with pkgs;
       (optional cfg.enableSatisfactory ficsit-cli)
-      ++ (optional cfg.enableThunderstore gale);
-    # ++ (optional cfg.enableBeatSaber beatsabermodmanager);
+      ++ (optional cfg.enableBeatSaber beatsabermodmanager)
+      ++ (optionals cfg.enableThunderstore [
+        gale
+        r2modman
+      ])
+      ++ (optional cfg.enableNexus (
+        nexusmods-app.override {
+          _7zz = _7zz-rar;
+        }
+      ));
 
     xdg.mimeApps.defaultApplications = mkIf cfg.enableBeatSaber {
       "x-scheme-handler/beatsaver" = "BeatSaberModManager-url-beatsaver.desktop";
@@ -18,11 +37,15 @@
       "x-scheme-handler/bsplaylist" = "BeatSaberModManager-url-bsplaylist.desktop";
     };
 
-    user.persistence.directories = (optional cfg.enableSatisfactory ".local/share/ficsit")
+    user.persistence.directories =
+      (optional cfg.enableSatisfactory ".local/share/ficsit")
       ++ (optional cfg.enableBeatSaber ".config/BeatSaberModManager")
       ++ (optionals cfg.enableThunderstore [
-      ".config/com.kesomannen.gale"
-      ".local/share/com.kesomannen.gale"
-    ]);
+        ".config/r2modman"
+        ".config/r2modmanPlus-local"
+        ".config/com.kesomannen.gale"
+        ".local/share/com.kesomannen.gale"
+      ])
+      ++ (optional cfg.enableNexus ".local/share/NexusMods.App");
   };
 }
