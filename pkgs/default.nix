a48e3fe0de07dbf3a2af4bf10ef849fdf1cbd02f
DaRacci <me@racci.dev>
2025-01-29 19:48:42 +1100

    chore: remove packages that are now in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,8 +5,6 @@
 
   # VR Stuff
   alvr = pkgs.callPackage ./alvr { };
-  oscavmgr = pkgs.callPackage ./oscavmgr { };
-  vrcadvert = pkgs.callPackage ./vrcadvert { };
 
   # Racing Games
   monocoque = pkgs.callPackage ./monocoque { };
