e2563eb86914e83e72bdd7c658a817ac00b4c80a
DaRacci <me@racci.dev>
2025-01-29 19:47:42 +1100

    fix: patch oscavmgr until new release

--- overlays/default.nix
+++ overlays/default.nix
@@ -51,14 +51,8 @@ in
       in
       final.discord.override ({ withOpenASAR = true; } // nss);
 
-    cliphist = prev.cliphist.overrideAttrs (_old: {
-      src = final.fetchFromGitHub {
-        owner = "sentriz";
-        repo = "cliphist";
-        rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
-        sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
-      };
-      vendorHash = "sha256-M5n7/QWQ5POWE4hSCMa0+GOVhEDCOILYqkSYIGoy/l0=";
+    oscavmgr = prev.oscavmgr.overrideAttrs (_oldAttrs: {
+      patches = [ ./oscavmgr_header.patch ];
     });
 
     inherit lib;
