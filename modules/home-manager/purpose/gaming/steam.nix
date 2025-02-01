32ad95591b3d32cfafa47413ae0b3ba67c3b7de8
DaRacci <me@racci.dev>
2025-02-01 21:48:53 +1100

    refactor(steam): remove unrequired patches & add decky loader cef loader patch

--- modules/home-manager/purpose/gaming/steam.nix
+++ modules/home-manager/purpose/gaming/steam.nix
@@ -27,101 +27,16 @@ in
     };
 
     home = {
-      #region - Fix Big Picture Mode on Nvidia
-      file = {
-        ".config/steam-flags-blocklist.conf" = {
-          text = ''
-            --disable-gpu
-            --disable-gpu-compositing
-            --use-angle=gl
-            --disable-smooth-scrolling
-          '';
-        };
+      activation.decky-loader-enabler = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        FILE="$HOME/.local/share/Steam/.cef-enable-remote-debugging"
 
-        ".config/steam-flags.conf" = mkIf cfg.enableNvidiaPatches {
-          text = ''
-            --ignore-gpu-blocklist
-            --disable-frame-rate-limit
-            --enable-gpu-rasterization
-            --enable-features=VaapiVideoDecoder
-            --use-gl=desktop
-            --enable-zero-copy
-          '';
-        };
-      };
-
-      # Applies a fix to the run script that allows gpu acceleration to work on nvidia
-      # Implemented from https://github.com/ValveSoftware/steam-for-linux/issues/8918#issuecomment-1574456384
-      activation.steam-fixer =
-        let
-          file = "${config.home.homeDirectory}/.local/share/Steam/ubuntu12_64/steam-runtime-heavy/run.sh";
-          fixString = "# CFG FIX BELOW";
-          remoteArchive = fetchGit {
-            url = "https://github.com/ValveSoftware/steam-runtime.git";
-            ref = "master";
-          };
-          actualFix = ''
-            # Not steamwebhelper so skip
-            if [[ "$1" != *steamwebhelper* ]]; then
-              exec "$@"
-              exit
-            fi
-
-            args=()
-
-            # Read blocklist from ~/.config/steam-flag-blocklist.conf
-            blocklisted_flags=()
-            while read flag; do
-                blocklisted_flags+=("$flag")
-            done < "$XDG_CONFIG_HOME/steam-flags-blocklist.conf"
-
-            # Filter arguments using the blocklist
-            for arg in "$@"; do
-              include_arg=true
-
-              for blocklisted_flag in "''${blocklisted_flags[@]}"; do
-                if [[ "$arg" == "$blocklisted_flag" ]]; then
-                  include_arg=false
-                fi
-              done
-
-              if $include_arg; then
-                args+=("$arg")
-              fi
-            done
-
-            # Add additional flags from ~/.config/steam-flags.conf
-            while read flag; do
-                args+=("$flag")
-            done < "$XDG_CONFIG_HOME/steam-flags.conf"
-
-            # Execute
-            echo "''${args[@]}" >> /tmp/steam-args
-            exec "''${args[@]}"
-          '';
-        in
-        mkIf cfg.enableNvidiaPatches (
-          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-            fix () {
-              echo "Applying steam fixer"
-              sed 's,exec "$@",${fixString},g' -i '${file}'
-              echo '${actualFix}' >> '${file}'
-            }
-
-            if [ -f '${file}' ]; then
-              echo "Steam runtime found"
-              if grep -q '${fixString}' '${file}'; then
-                echo "Steam fixer already applied"
-              else
-                fix
-              fi;
-            else
-              cp '${remoteArchive.outPath}/templates/run.sh' '${file}'
-              fix
-            fi;
-          ''
-        );
-      #endregion - Fix Big Picture Mode on Nvidia
+        if [ -f $FILE ]; then
+          echo "CEF Remote Debugging already enabled"
+        else
+          echo "Enabling CEF Remote Debugging"
+          touch $FILE
+        fi
+      '';
 
       packages = with pkgs; [
         protonup-rs
@@ -129,20 +44,25 @@ in
       ];
     };
 
-    user.persistence.directories = [
-      {
-        directory = ".local/share/Steam";
-        method = "symlink";
-      }
-      ".config/steamtinkerlaunch"
-      ".config/AdwSteamGtk"
+    user.persistence = {
+      files = [
+        ".steam/registry.vdf"
+      ];
+      directories = [
+        {
+          directory = ".local/share/Steam";
+          method = "symlink";
+        }
+        ".config/steamtinkerlaunch"
+        ".config/AdwSteamGtk"
 
-      # Games
-      ".barony"
-      ".local/share/Colossal Order/Cities_Skylines"
-      ".config/WarThunder"
-      ".config/Gaijin"
-      ".config/unity3d/IronGate/Valheim"
-    ];
+        # Games
+        ".barony"
+        ".local/share/Colossal Order/Cities_Skylines"
+        ".config/WarThunder"
+        ".config/Gaijin"
+        ".config/unity3d/IronGate/Valheim"
+      ];
+    };
   };
 }
