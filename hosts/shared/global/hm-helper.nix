0a948bfb56ea097525cc65abdf7a379515eb745e
DaRacci <me@racci.dev>
2025-03-09 17:41:56 +1100

    chore: some hm helper stuff

--- hosts/shared/global/hm-helper.nix
+++ hosts/shared/global/hm-helper.nix
@@ -7,15 +7,17 @@
 let
   inherit (lib) mkIf;
 
+  hmUserAttrs = builtins.attrValues config.home-manager.users;
   hmUsers = builtins.filter (user: (builtins.hasAttr user config.home-manager.users)) (
     builtins.attrNames config.users.users
   );
   hasPackage = pkg: username: builtins.elem pkg config.home-manager.users.${username}.home.packages;
   usersWithPackage = pkg: builtins.filter (username: hasPackage pkg username) hmUsers;
   anyoneHasPackage = pkg: builtins.length (usersWithPackage pkg) > 0;
+  anyoneHasOption = userFilter: builtins.length (builtins.filter userFilter hmUserAttrs) > 0;
 in
 {
-  services.gnome.sushi.enable = anyoneHasPackage pkgs.sushi;
+  services.gnome.sushi.enable = anyoneHasPackage pkgs.nautilus;
   environment.pathsToLink = mkIf (anyoneHasPackage pkgs.nautilus) [
     "/share/nautilus-python/extensions"
   ];
@@ -30,5 +32,15 @@ in
         enable = builtins.length withPackage > 0;
         polkitPolicyOwners = withPackage;
       };
+  };
+
+  networking.firewall = lib.mkIf (anyoneHasOption (user: user.services.kdeconnect.enable)) rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
   };
 }
