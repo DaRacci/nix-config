afc173e4840d567542a64df03304dac51216119c
DaRacci <me@racci.dev>
2025-03-15 20:32:55 +1100

    refactor(desktop/global): use builtin nixos module

--- hosts/desktop/shared/appimage.nix
+++ hosts/desktop/shared/appimage.nix
@@ -1,23 +1,6 @@
-# TODO - Use the programs.appimage option when moving to release 24.05
-{ pkgs, lib, ... }:
-let
-  package = pkgs.appimage-run.override {
-    extraPkgs =
-      pkgs: with pkgs; [
-        libthai
-        libxkbcommon
-      ];
-  };
-in
 {
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = lib.getExe package;
-    recognitionType = "magic";
-    offset = 0;
-    mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
-    magicOrExtension = "\\x7fELF....AI\\x02";
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
   };
-
-  environment.systemPackages = [ package ];
 }
