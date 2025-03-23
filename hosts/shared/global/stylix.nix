6dedf16a02287d1830640f6b64ac5a83bb9722d4
DaRacci <me@racci.dev>
2025-03-23 14:36:42 +1100

    fix: this still needs to be defined i guess.

--- hosts/shared/global/stylix.nix
+++ hosts/shared/global/stylix.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   inputs,
   config,
   ...
@@ -9,5 +10,9 @@
   stylix = {
     enable = !config.host.device.isHeadless;
     polarity = "dark";
+    image = pkgs.fetchurl {
+      url = "https://nextcloud.racci.dev/s/Hy8qkAWYwqSTjKp/download/17.jpeg";
+      sha256 = "sha256-dYkMPG/kTs0zgwHDFGkzN4gwFqefDWfMC9RUSBVezXE=";
+    };
   };
 }
