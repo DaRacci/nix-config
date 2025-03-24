a219a79ab342a0669eae7ddd15777c3dab2865a3
DaRacci <me@racci.dev>
2025-03-24 21:57:58 +1100

    refactor: this module is now empty for kernel > 6.8

--- hosts/desktop/nixmi/hardware.nix
+++ hosts/desktop/nixmi/hardware.nix
@@ -8,7 +8,6 @@
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
     inputs.nixos-hardware.nixosModules.common-pc-ssd
-    inputs.nixos-hardware.nixosModules.common-hidpi
   ];
 
   hardware = {
