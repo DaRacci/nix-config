fb81e84bf70c0281407d93a5a9d85ecbb3fad873
DaRacci <me@racci.dev>
2025-03-30 23:14:11 +1100

    fix: im going to commit die

--- hosts/server/shared/uptime.nix
+++ hosts/server/shared/uptime.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   sops.secrets."UPGRADE_STATUS_ID" = { };
 
@@ -22,36 +27,38 @@
         UNIQUE_ID_FILE = config.sops.secrets.UPGRADE_STATUS_ID.path;
       };
 
-      serviceConfig.ExecStart = pkgs.writeShellApplication {
-        name = "upgrade-status";
-        runtimeInputs = [
-          pkgs.curl
-          pkgs.perl
-          pkgs.perlPackages.URIEscapeXS
-        ];
-        text = ''
-          URL="$UPTIME_ENDPOINT/$(cat "$UNIQUE_ID_FILE")"
-          STATUS=$(systemctl is-active nixos-upgrade.service)
+      serviceConfig.ExecStart = lib.getExe (
+        pkgs.writeShellApplication {
+          name = "upgrade-status";
+          runtimeInputs = [
+            pkgs.curl
+            pkgs.perl
+            pkgs.perlPackages.URIEscapeXS
+          ];
+          text = ''
+            URL="$UPTIME_ENDPOINT/$(cat "$UNIQUE_ID_FILE")"
+            STATUS=$(systemctl is-active nixos-upgrade.service)
 
-          function url_encode() {
-            echo -n "$1" | perl -MURI::Escape::XS -e 'print encodeURIComponent(<STDIN>);'
-          }
+            function url_encode() {
+              echo -n "$1" | perl -MURI::Escape::XS -e 'print encodeURIComponent(<STDIN>);'
+            }
 
-          if [ "$STATUS" = "failed" ]; then
-            LOG=$(url_encode "$(journalctl -u nixos-upgrade --lines=10 --no-pager)")
-            URL="$URL?status=down&msg=$LOG&ping="
-          else
-            LAST_PROFILE_DATE=$(nix profile history --profile /nix/var/nix/profiles/system | grep '^Version' | awk -F'[()]+' '{print $2}' | tail -n 1)
-            MSG=$(url_encode "Last upgrade: $LAST_PROFILE_DATE")
-            echo "Last upgrade: $LAST_PROFILE_DATE"
-            echo "Encoded message: $MSG"
-            URL="$URL?status=up&msg=$MSG&ping="
-          fi
+            if [ "$STATUS" = "failed" ]; then
+              LOG=$(url_encode "$(journalctl -u nixos-upgrade --lines=10 --no-pager)")
+              URL="$URL?status=down&msg=$LOG&ping="
+            else
+              LAST_PROFILE_DATE=$(nix profile history --profile /nix/var/nix/profiles/system | grep '^Version' | awk -F'[()]+' '{print $2}' | tail -n 1)
+              MSG=$(url_encode "Last upgrade: $LAST_PROFILE_DATE")
+              echo "Last upgrade: $LAST_PROFILE_DATE"
+              echo "Encoded message: $MSG"
+              URL="$URL?status=up&msg=$MSG&ping="
+            fi
 
-          echo "Sending request to $URL"
-          curl -s "$URL"
-        '';
-      };
+            echo "Sending request to $URL"
+            curl -s "$URL"
+          '';
+        }
+      );
     };
   };
 }
