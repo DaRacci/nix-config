3fe05d0a6a526025230b1893fea40490c6cdcc0d
DaRacci <me@racci.dev>
2025-04-03 19:46:32 +1100

    chore: no need for all the comments

--- home/racci/features/desktop/hyprland/looks.nix
+++ home/racci/features/desktop/hyprland/looks.nix
@@ -12,26 +12,26 @@
       enabled = true;
 
       bezier = [
-        "fluent_decel, 0, 0.2, 0.4, 1"
+        "fluentDecel, 0, 0.2, 0.4, 1"
         "easeOutCirc, 0, 0.55, 0.45, 1"
         "easeOutCubic, 0.33, 1, 0.68, 1"
-        "easeinoutsine, 0.37, 0, 0.63, 1"
+        "easeInOutSine, 0.37, 0, 0.63, 1"
       ];
 
+      # https://wiki.hyprland.org/Configuring/Animations/#animation-tree
       animation = [
-        "windowsIn, 1, 3, easeOutCubic, popin 30" # window open
-        "windowsOut, 1, 3, fluent_decel, popin 70" # window close.
-        "windowsMove, 1, 2, easeinoutsine, slide" # everything in between, moving, dragging, resizing.
+        "windowsIn, 1, 3, easeOutCubic, popin 30"
+        "windowsOut, 1, 3, fluentDecel, popin 70"
+        "windowsMove, 1, 2, easeInOutSine, slide"
 
-        # Fade
-        "fadeIn, 1, 3, easeOutCubic" # fade in (open) -> layers and windows
-        "fadeOut, 1, 2, easeOutCubic" # fade out (close) -> layers and windows
-        "fadeSwitch, 0, 1, easeOutCirc" # fade on changing activewindow and its opacity
-        "fadeShadow, 1, 10, easeOutCirc" # fade on changing activewindow for shadows
-        "fadeDim, 1, 4, fluent_decel" # the easing of the dimming of inactive windows
-        "border, 1, 2.7, easeOutCirc" # for animating the border's color switch speed
-        "borderangle, 1, 30, fluent_decel, once" # for animating the border's gradient angle - styles: once (default), loop
-        "workspaces, 1, 4, easeOutCubic, fade" # styles: slide, slidevert, fade, slidefade, slidefadevert
+        "fadeIn, 1, 3, easeOutCubic"
+        "fadeOut, 1, 2, easeOutCubic"
+        "fadeSwitch, 0, 1, easeOutCirc"
+        "fadeShadow, 1, 10, easeOutCirc"
+        "fadeDim, 1, 4, fluentDecel"
+        "border, 1, 2.7, easeOutCirc"
+        "borderangle, 1, 30, fluentDecel, once"
+        "workspaces, 1, 4, easeOutCubic, fade"
       ];
     };
 
