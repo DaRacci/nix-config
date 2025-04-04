78826e9c12cd96cef09c1b18c086fb425abfea91
DaRacci <me@racci.dev>
2025-04-05 00:01:53 +1100

    chore(racci/hyprland): I've actually never used these plugins

--- home/racci/features/desktop/hyprland/default.nix
+++ home/racci/features/desktop/hyprland/default.nix
@@ -253,30 +253,6 @@ with lib;
 
         middle_click_paste = false;
       };
-
-      plugin = {
-        overview = {
-          centerAligned = true;
-          hideTopLayers = true;
-          hideOverlayLayers = true;
-          showNewWorkspace = true;
-          exitOnClick = true;
-          exitOnSwitch = true;
-          drawActiveWorkspace = true;
-          reverseSwipe = true;
-        };
-
-        hyprexpo = {
-          columns = 3;
-          gap_size = 5;
-          bg_col = "rgb(000000)";
-          workspace_method = "first 1"; # [center/first] [workspace] e.g. first 1 or center m+1
-
-          enable_gesture = true; # laptop touchpad, 4 fingers
-          gesture_distance = 300; # how far is the "max"
-          gesture_positive = false;
-        };
-      };
     };
 
     extraConfig =
