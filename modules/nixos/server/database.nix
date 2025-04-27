7372165d5e5332ff114fdf4d0a71a92e7404a1a6
DaRacci <me@racci.dev>
2025-04-27 18:21:00 +1000

    fix(host/server): getNixioConfig logic

--- modules/nixos/server/database.nix
+++ modules/nixos/server/database.nix
@@ -10,7 +10,13 @@ let
 
   isNixio = config.host.name == "nixio";
   nixioConfig = flake.nixosConfigurations.nixio.config;
-  getNixioConfig = path: if isNixio then config.${path} else nixioConfig.${path};
+  getNixioConfig =
+    attrPath:
+    let
+      configuration = if isNixio then config else nixioConfig;
+      attrs = lib.splitString "." attrPath;
+    in
+    lib.lists.foldl' (acc: attr: acc.${attr}) configuration attrs;
 
   postgresHost = if isNixio then "localhost" else "nixio";
   postgresPort = getNixioConfig "services.postgresql.settings.port";
