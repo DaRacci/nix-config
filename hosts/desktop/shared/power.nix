46c04dec8f7672709b160306e54a3a4cf47f91df
DaRacci <me@racci.dev>
2025-05-10 22:10:23 +1000

    fix(hosts/desktop/winix): eval errors again

--- hosts/desktop/shared/power.nix
+++ hosts/desktop/shared/power.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 {
-  services.upower.enable = true;
-  services.power-profiles-daemon.enable = true;
+  services.upower.enable = lib.mkDefault true;
+  services.power-profiles-daemon.enable = lib.mkDefault true;
 
   powerManagement = {
     enable = lib.mkDefault true;
