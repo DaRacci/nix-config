f23a639c58fa11d2aad4806c308854515bd174f4
DaRacci <me@racci.dev>
2025-05-11 14:16:29 +1000

    feat(hosts/server/nixcloud): hass ui & nix configs at same time

--- hosts/server/nixcloud/home-assistant/default.nix
+++ hosts/server/nixcloud/home-assistant/default.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ./postgresql.nix
@@ -18,16 +22,26 @@
   services = {
     home-assistant = {
       enable = true;
-      openFirewall = true;
       package = pkgs.home-assistant.override { extraPackages = ps: [ ps.psycopg2 ]; };
-
-      isal = { };
 
       configWritable = true;
       config = {
         frontend = { };
         history = { };
+
+        isal = { };
+
+        "automation manual" = [ ];
+        "automation ui" = "!include automations.yaml";
+        "scene manual" = [ ];
+        "scene ui" = "!include scenes.yaml";
       };
     };
   };
+
+  # https://nixos.wiki/wiki/Home_Assistant#Combine_declarative_and_UI_defined_automations
+  systemd.tmpfiles.rules = [
+    "f ${config.services.home-assistant.configDir}/automations.yaml 0755 hass hass"
+    "f ${config.services.home-assistant.configDir}/scenes.yaml 0755 hass hass"
+  ];
 }
