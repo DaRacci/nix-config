661b3ae431dccb0770204f244a1b2ff6a2bde20f
DaRacci <me@racci.dev>
2025-06-01 15:54:06 +1000

    fix(ci): i guess this needs no-pure-eval

--- utils/build-vscode-extensions.sh
+++ utils/build-vscode-extensions.sh
@@ -15,10 +15,10 @@ in
 EOF
 )
 
-extensions=$(nix eval --json --expr "$LET_IMPORT_EXTENSIONS extensions")
+extensions=$(nix eval --no-pure-eval --json --expr "$LET_IMPORT_EXTENSIONS extensions")
 mapfile -t extensions < <(echo "$extensions" | jq -r 'to_entries | map(.key as $author | .value | to_entries | map($author + "." + .key)) | flatten | join("\n")')
 
 for extension in "${extensions[@]}"; do
   echo "Building $extension..."
-  nix build --no-link --expr "$LET_IMPORT_EXTENSIONS extensions.${extension}"
+  nix build --no-pure-eval --no-link --expr "$LET_IMPORT_EXTENSIONS extensions.${extension}"
 done
