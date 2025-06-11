a99d02dd0b9739140b7402076cca4866a37ca497
DaRacci <me@racci.dev>
2025-06-11 16:42:18 +1000

    fix: missing icon & blank window fix

--- pkgs/orca-slicer-zink/default.nix
+++ pkgs/orca-slicer-zink/default.nix
@@ -14,10 +14,13 @@ stdenv.mkDerivation {
   phases = [ "installPhase" ];
   installPhase = ''
     mkdir -p $out/{bin,share/applications}
+
     cp ${lib.getExe orca-slicer} $out/bin/orca-slicer-zink
+    cp -r ${orca-slicer}/share/icons $out/share
     cp ${orca-slicer}/share/applications/OrcaSlicer.desktop $out/share/applications/OrcaSlicer-zink.desktop
 
     wrapProgram $out/bin/orca-slicer-zink \
+      --set GBM_BACKEND dri \
       --set __GLX_VENDOR_LIBRARY_NAME mesa \
       --set __EGL_VENDOR_LIBRARY_FILENAMES ${mesa}/share/glvnd/egl_vendor.d/50_mesa.json \
       --set MESA_LOADER_DRIVER_OVERRIDE zink \
