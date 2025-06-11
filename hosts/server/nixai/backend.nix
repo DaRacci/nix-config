a69f8348a04c26f88ec2c5317559a85f875a1daa
DaRacci <me@racci.dev>
2025-06-11 19:48:53 +1000

    fix(hosts/nixai): no rocm

--- hosts/server/nixai/backend.nix
+++ hosts/server/nixai/backend.nix
@@ -3,8 +3,10 @@
   ...
 }:
 {
+  # Runs like shit because iGPU ROCM doesn't work
+  # Waiting for https://github.com/ollama/ollama/issues/2033 so it can run under vulkan
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-rocm;
+    package = pkgs.ollama;
   };
 }
