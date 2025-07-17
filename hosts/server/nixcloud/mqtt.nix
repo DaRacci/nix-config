1790ef002c04913b8475c7465e8efe82ca59d53a
DaRacci <me@racci.dev>
2025-07-17 22:40:07 +1000

    feat(hosts/nixcloud): add zigbee2mqtt

--- hosts/server/nixcloud/mqtt.nix
+++ hosts/server/nixcloud/mqtt.nix
@@ -4,31 +4,50 @@
 }:
 {
   sops.secrets."MQTT_ROOT_PASSWORD" = { };
-
-  services.mosquitto = {
-    enable = true;
-    logType = [ "all" ];
-    listeners = [
-      {
-        address = "127.0.0.1";
-        users.root = {
-          acl = [ "readwrite #" ];
-          passwordFile = config.sops.secrets."MQTT_ROOT_PASSWORD".path;
-        };
-      }
-    ];
-  };
-
-  server.proxy.virtualHosts.mqtt = rec {
-    ports = [ l4.listenPort ];
-    l4 = {
-      listenPort = 1883;
-      config = ''
-        route {
-          tls
-          proxy localhost:${toString l4.listenPort}
+  services = {
+    mosquitto = {
+      enable = true;
+      logType = [ "all" ];
+      listeners = [
+        {
+          address = "127.0.0.1";
+          users.root = {
+            acl = [ "readwrite #" ];
+            passwordFile = config.sops.secrets."MQTT_ROOT_PASSWORD".path;
+          };
         }
-      '';
+      ];
+    };
+
+    zigbee2mqtt = {
+      enable = true;
+      settings = {
+        homeassistant = true;
+        mqtt.server = "mqtt://localhost:1883";
+        serial.port = "/dev/ttyACM0";
+        frontend.enabled = true;
+      };
     };
   };
+
+  server.proxy.virtualHosts = {
+    mqtt = rec {
+      ports = [ l4.listenPort ];
+      l4 = {
+        listenPort = 1883;
+        config = ''
+          route {
+            tls
+            proxy localhost:${toString l4.listenPort}
+          }
+        '';
+      };
+    };
+
+    zigbee2mqtt.extraConfig = ''
+      reverse_proxy http://localhost:8080
+    '';
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8080 ];
 }
