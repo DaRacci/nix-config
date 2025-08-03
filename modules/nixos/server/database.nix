bb7ed833ee1fdb3f36eecba8903083cdaaea7e84
DaRacci <me@racci.dev>
2025-08-03 16:48:26 +1000

    refactor(hosts/nixai): use postgres as database backend for webui

--- modules/nixos/server/database.nix
+++ modules/nixos/server/database.nix
@@ -39,6 +39,13 @@ let
           item != null
       ))
     ];
+
+  allDatabaseNames = lib.pipe serverConfigurations [
+    (builtins.map (cfg: builtins.attrNames cfg.server.database.postgres))
+    builtins.concatLists
+  ];
+  hasPostgresDatabases = builtins.length (builtins.attrNames cfg.postgres) > 0;
+  hasRedisInstances = builtins.length (builtins.attrNames cfg.redis) > 0;
 in
 {
   options.server.database = with types; {
@@ -82,6 +89,7 @@ in
                       default =
                         config.sops.secrets."POSTGRES/${
                           lib.toUpper config.server.database.postgres.${name}.database
+                          |> builtins.replaceStrings [ "-" ] [ "_" ]
                         }_PASSWORD".path;
                       readOnly = true;
                     };
@@ -103,41 +111,128 @@ in
         )
       );
     };
+    redis = mkOption {
+      default = { };
+      type = attrsOf (
+        submodule (
+          { name, ... }:
+          {
+            options = {
+              host = mkOption {
+                type = str;
+                default = "localhost";
+                readOnly = true;
+              };
+
+              port = mkOption {
+                type = int;
+                default = 6379;
+                readOnly = true;
+              };
+
+              password = mkOption {
+                type = nullOr str;
+                default = null;
+              };
+            };
+          }
+        )
+      );
+    };
   };
 
-  config = lib.mkIf (isNixio || (builtins.length (builtins.attrNames cfg.postgres)) > 0) {
-    assertions =
-      (
-        if isNixio then
-          [
-            (
-              let
-                allDatabaseNames = lib.pipe serverConfigurations [
-                  (builtins.map (cfg: builtins.attrNames cfg.server.database.postgres))
-                  builtins.concatLists
-                ];
-                duplicateNames = lib.pipe allDatabaseNames [
-                  (builtins.groupBy (pair: pair))
-                  (lib.filterAttrs (_: names: (builtins.length names) > 1))
-                  (lib.mapAttrsToList (name: _: name))
-                ];
-              in
-              {
-                assertion = builtins.length duplicateNames == 0;
-                message = ''
-                  Duplicate database names found: ${builtins.toString duplicateNames}
-                '';
-              }
-            )
-          ]
-        else
-          [ ]
-      )
-      ++ [
+  config = lib.mkMerge [
+    (lib.mkIf (isNixio && hasPostgresDatabases) {
+      assertions = [
+        (
+          let
+            duplicateDatabaseNames =
+              allDatabaseNames
+              |> builtins.groupBy (v: v)
+              |> lib.filterAttrs (_: names: (builtins.length names) > 1)
+              |> builtins.attrNames;
+          in
+          {
+            assertion = builtins.length duplicateDatabaseNames == 0;
+            message = "Duplicate database names found: ${builtins.toString duplicateDatabaseNames}";
+          }
+        )
+      ];
+
+      services.postgresql = rec {
+        ensureDatabases =
+          serverConfigurations
+          |> builtins.map (cfg: builtins.attrNames cfg.server.database.postgres)
+          |> lib.flatten;
+
+        ensureUsers = builtins.map (database: {
+          name = database;
+          ensureDBOwnership = true;
+        }) ensureDatabases;
+
+        # Some modules configure initialScripts for postgres so we should ensure they are executed
+        initialScript =
+          gatherAllInstances "services.postgresql.initialScript"
+          |> builtins.map (path: builtins.readFile path)
+          |> builtins.concatStringsSep "\n"
+          |> pkgs.writeText "init-postgresql-script";
+
+        settings = {
+          shared_preload_libraries =
+            gatherAllInstances "services.postgresql.settings.shared_preload_libraries"
+            |> builtins.map (preload: if lib.isString preload then [ preload ] else preload)
+            |> builtins.concatLists
+            |> lib.unique;
+        };
+
+        extensions =
+          ps:
+          serverConfigurations
+          |> builtins.filter (cfg: cfg.host.name != "nixio")
+          |> builtins.map (cfg: cfg.services.postgresql.extensions ps)
+          |> builtins.concatLists
+          |> lib.unique;
+      };
+
+      systemd.services.postgresql-setup = {
+        postStart = lib.mkAfter (
+          builtins.concatStringsSep "\n" (
+            (gatherAllInstances "systemd.services.postgresql-setup.postStart")
+            ++ (lib.pipe serverConfigurations [
+              (builtins.map (cfg: builtins.attrValues cfg.server.database.postgres))
+              builtins.concatLists
+              (builtins.map (database: lib.mine.mkPostgresRolePass database.user database.password.path))
+            ])
+          )
+        );
+
+        serviceConfig.ExecStartPost =
+          gatherAllInstances "systemd.services.postgresql-setup.serviceConfig.ExecStartPost"
+          |> builtins.concatLists;
+      };
+    })
+
+    (lib.mkIf (isNixio || hasPostgresDatabases) {
+      sops.secrets =
+        builtins.attrValues cfg.postgres
+        |> map (
+          db:
+          lib.nameValuePair
+            "POSTGRES/${lib.toUpper db.database |> builtins.replaceStrings [ "-" ] [ "_" ]}_PASSWORD"
+            {
+              owner = lib.mkIf (db.password.owner != null) db.password.owner;
+              group = lib.mkIf (db.password.group != null) db.password.group;
+            }
+        )
+        |> lib.listToAttrs;
+    })
+
+    (lib.mkIf (!isNixio && hasPostgresDatabases) {
+      assertions = [
         {
-          assertion = isNixio || !config.services.postgresql.enable;
+          assertion = !config.services.postgresql.enable;
           message = ''
-            PostgreSQL is enabled but you have configured databases for management with NixIO.
+            PostgreSQL is enabled & has databases configured, but you have configured databases for management with NixIO.
             If this is on purpose and you want NixIO to manage these, set `services.postgresl.enable` to `false`.
 
             Configured databases: ${builtins.toString config.services.postgresql.ensureDatabases}
@@ -145,84 +240,29 @@ in
         }
       ];
 
-    sops.secrets = lib.pipe cfg.postgres [
-      builtins.attrValues
-      (map (
-        database:
-        lib.nameValuePair "POSTGRES/${lib.toUpper database.database}_PASSWORD" (
-          lib.optionalAttrs (database.password.owner != null) {
-            inherit (database.password) owner;
-          }
-        )
-        // (lib.optionalAttrs (database.password.group != null) {
-          inherit (database.password) group;
-        })
-      ))
-      lib.listToAttrs
-    ];
+      services.postgresql.package = lib.mkDefault pkgs.postgresql_17;
+    })
 
-    services.postgresql =
-      lib.mergeAttrs
+    (lib.mkIf (isNixio && hasRedisInstances) {
+      services.redis.servers = {
+
+      };
+    })
+
+    (lib.mkIf (!isNixio && hasRedisInstances) {
+      assertions = [
         {
-          package = lib.mkDefault pkgs.postgresql_17;
+          assertion = !config.services.redis.enable;
+          message = ''
+            Redis is enabled & has instances, but you have configured databases for management with NixIO.
+            If this is on purpose and you want NixIO to manage these, set `services.redis.enable` to `false`.
+
+            Configured redis instances: ${
+              builtins.attrNames config.services.redis.servers |> builtins.concatStringsSep ", "
+            }
+          '';
         }
-        (
-          lib.optionalAttrs isNixio rec {
-            ensureDatabases = lib.pipe serverConfigurations [
-              (builtins.map (cfg: builtins.attrNames cfg.server.database.postgres))
-              lib.flatten
-            ];
-
-            ensureUsers = builtins.map (database: {
-              name = database;
-              ensureDBOwnership = true;
-            }) ensureDatabases;
-
-            # Some modules configure initialScripts for postgres so we should ensure they are executed
-            initialScript = lib.mkIf isNixio (
-              lib.pipe (gatherAllInstances "services.postgresql.initialScript") [
-                (builtins.map (path: builtins.readFile path))
-                (builtins.concatStringsSep "\n")
-                (pkgs.writeText "init-postgresql-script")
-              ]
-            );
-
-            settings = {
-              shared_preload_libraries =
-                lib.pipe (gatherAllInstances "services.postgresql.settings.shared_preload_libraries")
-                  [
-                    (builtins.map (preload: if lib.isString preload then [ preload ] else preload))
-                    builtins.concatLists
-                    lib.unique
-                  ];
-            };
-
-            extensions =
-              ps:
-              lib.pipe serverConfigurations [
-                (builtins.filter (cfg: cfg.host.name != "nixio"))
-                (builtins.map (cfg: cfg.services.postgresql.extensions ps))
-                builtins.concatLists
-                lib.unique
-              ];
-          }
-        );
-
-    systemd.services.postgresql-setup = lib.optionalAttrs isNixio {
-      postStart = lib.mkAfter (
-        builtins.concatStringsSep "\n" (
-          (gatherAllInstances "systemd.services.postgresql-setup.postStart")
-          ++ (lib.pipe serverConfigurations [
-            (builtins.map (cfg: builtins.attrValues cfg.server.database.postgres))
-            builtins.concatLists
-            (builtins.map (database: lib.mine.mkPostgresRolePass database.user database.password.path))
-          ])
-        )
-      );
-
-      serviceConfig.ExecStartPost =
-        gatherAllInstances "systemd.services.postgresql-setup.serviceConfig.ExecStartPost"
-        |> builtins.concatLists;
-    };
-  };
+      ];
+    })
+  ];
 }
