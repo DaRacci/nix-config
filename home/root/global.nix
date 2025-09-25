f1dea2f9b0225e15669cf4221b62562cb0d580d1
DaRacci <me@racci.dev>
2025-09-26 09:07:58 +1000

    refactor(home/root): use helix as editor

--- home/root/global.nix
+++ home/root/global.nix
@@ -4,8 +4,14 @@
 
   user.persistence.enable = false;
 
+  # Minimising unnecessary packages
   manual.manpages.enable = false;
   programs.man.enable = false;
   fonts.fontconfig.enable = false;
   stylix.enable = false;
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+  };
 }
