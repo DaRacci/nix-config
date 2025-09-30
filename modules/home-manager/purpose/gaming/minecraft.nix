7e1d7deba55643bdcee907a1809891ab3f6a5656
DaRacci <me@racci.dev>
2025-09-30 18:16:25 +1000

    feat(modules/home-manager): minecraft module

--- modules/home-manager/purpose/gaming/minecraft.nix
+++ modules/home-manager/purpose/gaming/minecraft.nix
@@ -1,4 +1,24 @@
-# TODO
-_: {
-  # home.packages = with pkgs; [ modrinth-app ];
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.purpose.gaming.minecraft;
+in
+{
+  options.purpose.gaming.minecraft = {
+    enable = lib.mkEnableOption "Enable Minecraft support";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gdlauncher-carbon
+    ];
+
+    user.persistence.directories = [
+      ".local/share/gdlauncher_carbon"
+    ];
+  };
 }
