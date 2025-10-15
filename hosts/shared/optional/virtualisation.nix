a6bf23b5e1d666aa388c44b96880b4d8fb612591
DaRacci <me@racci.dev>
2025-10-15 16:30:49 +1100

    fix(hosts/shared): ovmf submodule is gone need this workaround for now

--- hosts/shared/optional/virtualisation.nix
+++ hosts/shared/optional/virtualisation.nix
@@ -62,16 +62,16 @@ in
       qemu = {
         runAsRoot = false;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMFFull.override {
-              secureBoot = true;
-              tpmSupport = true;
-              msVarsTemplate = true;
-            }).fd
-          ];
-        };
+        #ovmf = {
+        #  enable = true;
+        #  packages = [
+        #    (pkgs.OVMFFull.override {
+        #      secureBoot = true;
+        #      tpmSupport = true;
+        #      msVarsTemplate = true;
+        #    }).fd
+        #  ];
+        #        };
 
         verbatimConfig = ''
           cgroup_device_acl = [
@@ -92,12 +92,31 @@ in
   };
 
   systemd = {
-    services."libvirt-nosleep@" = {
-      description = ''Preventing sleep while libvirt domain "%i" is running'';
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = ''${pkgs.systemd}/bin/systemd-inhibit --what=sleep --why="Libvirt domain "%i" is running" --who=%U --mode=block sleep infinity'';
+    services = {
+      "libvirt-nosleep@" = {
+        description = ''Preventing sleep while libvirt domain "%i" is running'';
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = ''${pkgs.systemd}/bin/systemd-inhibit --what=sleep --why="Libvirt domain "%i" is running" --who=%U --mode=block sleep infinity'';
+        };
       };
+
+      libvirtd-config.script =
+        let
+          ovmfPackage = pkgs.OVMFFull.override {
+            secureBoot = true;
+            tpmSupport = true;
+            msVarsTemplate = true;
+          };
+        in
+        config.systemd.services.libvirtd.script
+        + ''
+          ln -s --force ${ovmfPackage}/FV/AAVMF_CODE{,.ms}.fd /run/libvirt/nix-ovmf/
+          ln -s --force ${ovmfPackage}/FV/OVMF_CODE{,.ms}.fd /run/libvirt/nix-ovmf/
+          ln -s --force ${ovmfPackage}/FV/AAVMF_VARS{,.ms}.fd /run/libvirt/nix-ovmf/
+          ln -s --force ${ovmfPackage}/FV/OVMF_VARS{,.ms}.fd /run/libvirt/nix-ovmf/
+        '';
+
     };
 
     tmpfiles.rules =
