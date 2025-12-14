515b1942f53e3b80a1ee705924393baa04274a76
DaRacci <me@racci.dev>
2025-12-14 16:15:01 +1100

    refactor(home/root): only use bash for base server deloyments.

--- home/root/os-config.nix
+++ home/root/os-config.nix
@@ -8,11 +8,18 @@
 let
   inherit (lib) mkForce;
   # WSL is fucky with nu so we use fish instead, same with proxmox LXC.
-  useFish = builtins.hasAttr "wsl" config || config.host.device.role == "server";
+  useFish = builtins.hasAttr "wsl" config;
+  useNu = config.host.device.role != "server";
 in
 {
   users.users.root = {
-    shell = if useFish then pkgs.fish else pkgs.nushell;
+    shell =
+      if useFish then
+        pkgs.fish
+      else if useNu then
+        pkgs.nushell
+      else
+        pkgs.bash;
     isNormalUser = mkForce false;
 
     openssh.authorizedKeys.keyFiles = [ "${self}/home/racci/id_ed25519.pub" ];
