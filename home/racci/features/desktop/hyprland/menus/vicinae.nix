8f0130bfbb25b4f16e86f7df3a4a86d6cf40885d
DaRacci <me@racci.dev>
2026-01-16 17:50:23 +1100

    refactor(home/racci): vicinae settings :)

--- home/racci/features/desktop/hyprland/menus/vicinae.nix
+++ home/racci/features/desktop/hyprland/menus/vicinae.nix
@@ -1,13 +1,52 @@
 {
-  inputs,
+  pkgs,
   ...
 }:
 {
-  imports = [ inputs.vicinae.homeManagerModules.default ];
-
-  services.vicinae = {
+  programs.vicinae = {
     enable = true;
     systemd.enable = true;
+    settings = {
+      font.normal.size = 12;
+
+      providers = {
+        "@khasbilegt/store.raycast.1password" = {
+          preferences = {
+            cliPath = "/run/wrappers/bin/op";
+            zshPath = "${pkgs.zsh}";
+          };
+          entrypoints = {
+            generate-password.alias = "gp";
+            item-list.alias = "op";
+          };
+        };
+
+        "@knoopx/store.vicinae.nix".entrypoints = {
+          flake-packages.alias = "nf";
+          home-manager-options.alias = "hmo";
+          options.alias = "no";
+          packages.alias = "np";
+          pull-requests.alias = "npr";
+        };
+
+        clipboard.preferences = {
+          encryption = true;
+          ignorePasswords = false;
+        };
+
+        core.entrypoints = {
+          about.enabled = false;
+          documentation.enabled = false;
+          manage-fallback.enabled = false;
+          open-config-file.enabled = false;
+          open-default-config.enabled = false;
+          report-bug.enabled = false;
+          sponsor.enabled = false;
+        };
+        developer.enabled = false;
+        theme.enabled = false;
+      };
+    };
   };
 
   wayland.windowManager.hyprland.settings = {
