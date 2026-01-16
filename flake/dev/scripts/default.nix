a6b94a03156944953ce8a5b725378673bcfb4eb1
DaRacci <me@racci.dev>
2026-01-16 17:50:23 +1100

    ci: Convert GHA workflows to Woodpecker CI pipelines
    - Add ci.yaml for flake checks and affected output builds
    - Add packages.yaml for package building with static matrix
    - Add update-packages.yaml for automated package updates
    - Complete docker.yaml for lix-woodpecker image publishing

--- flake/dev/scripts/default.nix
+++ flake/dev/scripts/default.nix
@@ -5,25 +5,39 @@
 }:
 let
   inherit (lib.mine.packages) writeNuApplicationWithLibs;
+  libSource = ../../../lib/nu-lib;
+  sourceRoot = ./.;
 in
 {
+  build-changed = writeNuApplicationWithLibs {
+    inherit pkgs libSource sourceRoot;
+    name = "build-changed";
+  };
+
+  get-symbol-value = writeNuApplicationWithLibs {
+    inherit pkgs libSource sourceRoot;
+    name = "get-symbol-value";
+  };
+
+  module-graph = writeNuApplicationWithLibs {
+    inherit pkgs libSource sourceRoot;
+    name = "module-graph";
+  };
+
   nix-tree-host = writeNuApplicationWithLibs {
-    inherit pkgs;
-    sourceRoot = ./.;
+    inherit pkgs libSource sourceRoot;
     name = "nix-tree-host";
     runtimeInputs = [ pkgs.nix-tree ];
   };
 
   rebuild-target = writeNuApplicationWithLibs {
-    inherit pkgs;
-    sourceRoot = ./.;
+    inherit pkgs libSource sourceRoot;
     name = "rebuild-target";
     runtimeInputs = [ pkgs.nh ];
   };
 
   update-redis-mappings = writeNuApplicationWithLibs {
-    inherit pkgs;
-    sourceRoot = ./.;
+    inherit pkgs libSource sourceRoot;
     name = "update-redis-mappings";
     runtimeInputs = [ pkgs.nix ];
   };
