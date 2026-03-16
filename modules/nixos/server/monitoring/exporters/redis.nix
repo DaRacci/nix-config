2df1f1d3f92153d7847b5594ab7d6eb664991e3b
DaRacci <me@racci.dev>
2026-03-17 09:37:31 +1100

    fix(modules/nixos/server): redis exporter authentication

--- modules/nixos/server/monitoring/exporters/redis.nix
+++ modules/nixos/server/monitoring/exporters/redis.nix
@@ -22,14 +22,22 @@ in
         && config.services.redis.servers."".enable
       )
       {
+        sops.templates."redis-exporter-password".content = builtins.toJSON {
+          "reids://localhost:16379" = config.sops.placeholder."REDIS/PASSWORD";
+        };
+
         services.prometheus.exporters.redis = {
           enable = true;
           port = 9121;
           extraFlags = [
-            "--redis.password-file ${config.sops.secrets."REDIS/PASSWORD".path}"
+            "--redis.password-file %d/redis-password"
           ];
         };
 
         networking.firewall.allowedTCPPorts = [ 9121 ];
+
+        systemd.services.prometheus-redis-exporter.serviceConfig.LoadCredential = [
+          "redis-password:${config.sops.templates."redis-exporter-password".path}"
+        ];
       };
 }
