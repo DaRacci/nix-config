3c037777216314359c4375903a5161f69b312f51
DaRacci <me@racci.dev>
2026-04-21 20:48:53 +1000

    refactor(docs): Add build-time option embedding to docs

--- docs/default.nix
+++ docs/default.nix
@@ -6,6 +6,29 @@
   lib,
   ...
 }:
+let
+  inherit (inputs.search.packages.${system}) mkOptionsJSON;
+
+  # Build a minimal module evaluation suitable for options documentation.
+  # Mirrors the pattern used in search.nix so the same specialArgs are
+  # available to every module under modules/nixos.
+  mkModuleOptions =
+    path:
+    mkOptionsJSON {
+      modules = [
+        path
+        {
+          _module.args = {
+            inherit self inputs pkgs;
+          };
+        }
+      ];
+      specialArgs = {
+        inherit inputs;
+        users = [ ];
+      };
+    };
+in
 rec {
   search = pkgs.callPackage ./search.nix {
     inherit
@@ -17,6 +40,11 @@ rec {
     lib = pkgs.lib;
   };
 
+  # options.json for individual modules – used by site.nix to generate
+  # inline option reference docs (build-time) and client-side widgets.
+  woodpeckerNixOptionsJSON = mkModuleOptions "${self}/modules/nixos/services/woodpecker-nix.nix";
+  tailscaleOptionsJSON = mkModuleOptions "${self}/modules/nixos/services/tailscale.nix";
+
   docs = pkgs.callPackage ./site.nix {
     inherit
       self
@@ -24,6 +52,8 @@ rec {
       pkgs
       lib
       search
+      woodpeckerNixOptionsJSON
+      tailscaleOptionsJSON
       ;
   };
 
