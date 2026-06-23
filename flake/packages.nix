b4058c9231611635418a744ae506106acb118e2d
DaRacci <me@racci.dev>
2026-06-23 17:01:15 +1000

    fix: error due to non derivations added to packages

--- flake/packages.nix
+++ flake/packages.nix
@@ -25,15 +25,18 @@
         (import "${self}/flake/ci/scripts" { inherit inputs pkgs lib; })
         (import "${self}/flake/dev/scripts" { inherit inputs pkgs lib; })
 
-        (import "${self}/docs" {
-          inherit
-            self
-            system
-            pkgs
-            lib
-            ;
-          inputs = config.partitions.docs.extraInputs;
-        })
+        (
+          import "${self}/docs" {
+            inherit
+              self
+              system
+              pkgs
+              lib
+              ;
+            inputs = config.partitions.docs.extraInputs;
+          }
+          |> lib.filterAttrs (_: v: lib.isDerivation v)
+        )
       ];
     };
 }
