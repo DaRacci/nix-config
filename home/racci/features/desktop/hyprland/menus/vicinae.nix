0e2f7fe8e14545234ba9bc1216487b2c349ab1c1
DaRacci <me@racci.dev>
2026-09-12 16:34:08 +1000

    feat(vicinae): add encryption, custom dirs, indexing, and new providers
    Fixes the massive search index file sitting in memory filling like 8GB, it was scanning all Steam files, proton dirs, cargo target outputs etc.
    
    Fixes clipboard keytoggle using incorrect vicinae deeplink.

--- home/racci/features/desktop/hyprland/menus/vicinae.nix
+++ home/racci/features/desktop/hyprland/menus/vicinae.nix
@@ -1,21 +1,74 @@
-{
-  pkgs,
-  ...
-}:
-{
+_: {
   programs.vicinae = {
     enable = true;
     systemd.enable = true;
     settings = {
+      encrypt_sensitive_data = true;
+      developer.enabled = false;
+      theme.enabled = false;
+      power.enabled = false;
+
+      scripts.preferences.customDirs = [ "/home/racci/.local/bin" ];
+
+      clipboard = {
+        preferences = {
+          encryption = true;
+          ignorePasswords = false;
+        };
+        entrypoints = {
+          clear.enabled = false;
+          clear-history.enabled = false;
+        };
+      };
+
+      files.preferences = {
+        autoIndexing = true;
+        indexingPaths = [
+          "/home/racci/Templates"
+          "/home/racci/Pictures"
+          "/home/racci/Documents"
+          "/home/racci/Downloads"
+          "/home/racci/Videos"
+          "/home/racci/Projects"
+          "/home/racci/Music"
+        ];
+        excludedIndexingPaths = [
+          "/home/racci/Projects/Coding"
+          "/home/racci/Projects/AIFS"
+        ];
+      };
+
+      core.entrypoints = {
+        about.enabled = false;
+        documentation.enabled = false;
+        manage-fallback.enabled = false;
+        open-config-file.enabled = false;
+        open-default-config.enabled = false;
+        report-bug.enabled = false;
+        sponsor.enabled = false;
+      };
+
+      browser-extension.entrypoints = {
+        shortcut-active-tab.enabled = false;
+      };
+
+      system.entrypoints = {
+        browse-apps.enabled = false;
+        toggle-mute.enabled = false;
+        volume-0.enabled = false;
+        volume-25.enabled = false;
+        volume-50.enabled = false;
+        volume-75.enabled = false;
+        volume-100.enabled = false;
+        volume-up.enabled = false;
+        volume-down.enabled = false;
+      };
+
       providers = {
-        "@khasbilegt/store.raycast.1password" = {
+        "@Ninetonine/store.vicinae.searxng" = {
           preferences = {
-            cliPath = "/run/wrappers/bin/op";
-            zshPath = "${pkgs.zsh}";
-          };
-          entrypoints = {
-            generate-password.alias = "gp";
-            item-list.alias = "op";
+            instance_domain = "https://search.racci.dev";
+            languages = "en";
           };
         };
 
@@ -25,24 +78,12 @@
           options.alias = "no";
           packages.alias = "np";
           pull-requests.alias = "npr";
+          protondb-search.alias = "pdb";
         };
 
-        clipboard.preferences = {
-          encryption = true;
-          ignorePasswords = false;
+        "@xwaleedahmad/store.vicinae.kde-connect".entrypoints = {
+          send-clipboard.enabled = false;
         };
-
-        core.entrypoints = {
-          about.enabled = false;
-          documentation.enabled = false;
-          manage-fallback.enabled = false;
-          open-config-file.enabled = false;
-          open-default-config.enabled = false;
-          report-bug.enabled = false;
-          sponsor.enabled = false;
-        };
-        developer.enabled = false;
-        theme.enabled = false;
       };
     };
   };
