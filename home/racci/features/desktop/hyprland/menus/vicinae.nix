27227638b4500337c23b07f7c439e57fb8edf632
DaRacci <me@racci.dev>
2026-09-25 17:07:23 +1000

    fix(vicinae): nest provider settings under providers key

--- home/racci/features/desktop/hyprland/menus/vicinae.nix
+++ home/racci/features/desktop/hyprland/menus/vicinae.nix
@@ -3,68 +3,69 @@ _: {
     enable = true;
     systemd.enable = true;
     settings = {
+      telemetry.system_info = false;
       encrypt_sensitive_data = true;
       developer.enabled = false;
       theme.enabled = false;
       power.enabled = false;
 
-      scripts.preferences.customDirs = [ "/home/racci/.local/bin" ];
-
-      clipboard = {
-        preferences = {
-          encryption = true;
-          ignorePasswords = false;
-        };
-        entrypoints = {
-          clear.enabled = false;
-          clear-history.enabled = false;
-        };
-      };
-
-      files.preferences = {
-        autoIndexing = true;
-        indexingPaths = [
-          "/home/racci/Templates"
-          "/home/racci/Pictures"
-          "/home/racci/Documents"
-          "/home/racci/Downloads"
-          "/home/racci/Videos"
-          "/home/racci/Projects"
-          "/home/racci/Music"
-        ];
-        excludedIndexingPaths = [
-          "/home/racci/Projects/Coding"
-          "/home/racci/Projects/AIFS"
-        ];
-      };
-
-      core.entrypoints = {
-        about.enabled = false;
-        documentation.enabled = false;
-        manage-fallback.enabled = false;
-        open-config-file.enabled = false;
-        open-default-config.enabled = false;
-        report-bug.enabled = false;
-        sponsor.enabled = false;
-      };
-
-      browser-extension.entrypoints = {
-        shortcut-active-tab.enabled = false;
-      };
-
-      system.entrypoints = {
-        browse-apps.enabled = false;
-        toggle-mute.enabled = false;
-        volume-0.enabled = false;
-        volume-25.enabled = false;
-        volume-50.enabled = false;
-        volume-75.enabled = false;
-        volume-100.enabled = false;
-        volume-up.enabled = false;
-        volume-down.enabled = false;
-      };
-
       providers = {
+        scripts.preferences.customDirs = [ "/home/racci/.local/bin" ];
+
+        core.entrypoints = {
+          about.enabled = false;
+          documentation.enabled = false;
+          manage-fallback.enabled = false;
+          open-config-file.enabled = false;
+          open-default-config.enabled = false;
+          report-bug.enabled = false;
+          sponsor.enabled = false;
+        };
+
+        browser-extension.entrypoints = {
+          shortcut-active-tab.enabled = false;
+        };
+
+        system.entrypoints = {
+          browse-apps.enabled = false;
+          toggle-mute.enabled = false;
+          volume-0.enabled = false;
+          volume-25.enabled = false;
+          volume-50.enabled = false;
+          volume-75.enabled = false;
+          volume-100.enabled = false;
+          volume-up.enabled = false;
+          volume-down.enabled = false;
+        };
+
+        clipboard = {
+          preferences = {
+            encryption = true;
+            ignorePasswords = false;
+          };
+          entrypoints = {
+            clear.enabled = false;
+            clear-history.enabled = false;
+          };
+        };
+
+        files.preferences = {
+          autoIndexing = true;
+          indexingPaths = [
+            "/home/racci/Templates"
+            "/home/racci/Pictures"
+            "/home/racci/Documents"
+            "/home/racci/Downloads"
+            "/home/racci/Videos"
+            "/home/racci/Projects"
+            "/home/racci/Music"
+          ];
+          excludedIndexingPaths = [
+            "/home/racci/Projects/Coding"
+            "/home/racci/Projects/AIFS"
+          ];
+        };
+
         "@Ninetonine/store.vicinae.searxng" = {
           preferences = {
             instance_domain = "https://search.racci.dev";
